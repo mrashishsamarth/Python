d93ff19ed2ba88f0811f3272504b3d1183fc2751
mrashishsamarth <mrashishsamarth@gmail.com>
2021-03-03 14:15:41 -0800

    Initial Commit

--- Python--File-Data-Handling/File-Operations-Read-All-Lines/FileOperationsReadAllLines.py
+++ Python--File-Data-Handling/File-Operations-Read-All-Lines/FileOperationsReadAllLines.py
@@ -4,3 +4,14 @@ for line in lines:
     print(line)
 source_file.close()
 print("\n Reached - End of File. File is closed")
+
+'''
+:Output-
+
+#CustomerID|SubscriptionID|ProductModel
+
+ Reached - End of File. File is closed
+
+Process finished with exit code 0
+
+'''
