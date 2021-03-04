46757c50306906ee1dccddf82c9b9f04431f4fd4
mrashishsamarth <mrashishsamarth@gmail.com>
2021-03-03 20:04:33 -0800

    Initial Commit

--- Python--File-Data-Handling/With-Pandas-File-Validation-if-Column-exists-config-approach/With-Pandas-File-Validation-if-Column-exists-config-approach.py
+++ Python--File-Data-Handling/With-Pandas-File-Validation-if-Column-exists-config-approach/With-Pandas-File-Validation-if-Column-exists-config-approach.py
@@ -1,8 +1,6 @@
 import pandas as pd
 import column_list
 result_set = pd.read_csv(r'source_file/Machine_readable_file_bd_employ.csv', sep=',', encoding='utf8')
-# print(result_set.columns)
-column_exists = None
 for item in column_list.col:
     print("Does the column " + item + " exists in the CSV file ? :- " + str(item in result_set.columns))
 
