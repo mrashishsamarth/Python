b4f4adf6c87b0fdf88412f38c33586894bbc9f86
mrashishsamarth <mrashishsamarth@gmail.com>
2021-03-04 13:23:01 -0800

    Initial Commit

---
+++ Python--Selenium/0002-Open-Url-Get-Title/0002-Open-Url-Get-Title.py
@@ -0,0 +1,21 @@
+# Import time module, since we want the wait 5 seconds before closing the browser
+import time
+# Import web driver from selenium
+from selenium import webdriver
+
+# Following is the path for the chrome driver
+# Prefix 'r' is used to get rid of unicode errors due to path
+PATH = r"C:\Users\Groot\AppData\Local\Programs\Python\Python39\Scripts\chromedriver.exe"
+
+# Set the driver as the chromedriver.exe to control the browser
+driver = webdriver.Chrome(PATH)
+# As soon the browser is opened, maximize the window
+driver.maximize_window()
+# Navigate to the following url
+driver.get("https://www.oracle.com/database/technologies/appdev/rest-data-services-downloads.html#license-lightbox")
+# Wait for 5 seconds
+time.sleep(5)
+# Print the title of the browser window
+print(driver.title)
+# Quit the browser
+driver.quit()
