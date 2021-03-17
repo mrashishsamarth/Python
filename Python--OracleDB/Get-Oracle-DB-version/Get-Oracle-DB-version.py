db156a01994d75ccaa9d587c84279cadf9ee5ec7
mrashishsamarth <mrashishsamarth@gmail.com>
2021-03-16 19:45:06 -0700

    Initial Commit

--- Python--OracleDB/Get-Oracle-DB-version/Get-Oracle-DB-version.py
+++ Python--OracleDB/Get-Oracle-DB-version/Get-Oracle-DB-version.py
@@ -1,6 +1,6 @@
 import cx_Oracle
 # following example is from a locally installed Oracle DB
 connection = cx_Oracle.connect('SYSTEM/Black0ps@orcl')
-print("Python is connected to Oracle Version of " + connection.version
+print("Python is connected to Oracle Version of " + connection.version)
 
 connection.close()
